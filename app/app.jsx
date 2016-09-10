a5942ba274e6d37c6a7ed34450ae07906913069b
william-mcdermott <williammcdermott@Williams-MacBook-Pro.local>
2016-09-10 11:28:07 -0500

    initial commit

---
+++ app/app.jsx
@@ -0,0 +1,15 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
+
+//Load foundations
+$(document).foundation();
+
+// App css
+require('style!css!sass!applicationStyles')
+
+ReactDOM.render(
+  <p>Boilerplate 3 project</p>,
+  document.getElementById('app')
+);
