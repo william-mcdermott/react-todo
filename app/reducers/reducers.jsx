b4c10eeb336f2b1713d38fcfa45649b34b4eab63
william-mcdermott <williammcdermott@Williams-MacBook-Pro.local>
2017-01-28 20:00:09 -0600

    Add todos reducer and tests

--- app/reducers/reducers.jsx
+++ app/reducers/reducers.jsx
@@ -1,13 +1,15 @@
+var uuid = require('node-uuid');
+var moment = require('moment');
+
 export var searchTextReducer = (state = '', action) => {
   switch (action.type) {
     case 'SET_SEARCH_TEXT':
       return action.searchText;
     default:
       return state;
-  }
-}
+  };
+};
 
-// showCompletedReducer, default false, TOGGLE_SHOW_COMPLETED
 export var showCompletedReducer = (state = false, action) => {
   switch (action.type) {
     case 'TOGGLE_SHOW_COMPLETED':
@@ -15,4 +17,36 @@ export var showCompletedReducer = (state = false, action) => {
     default:
       return state;
   }
-}
+};
+
+export var todosReducer = (state = [], action) => {
+  switch (action.type) {
+    case 'ADD_TODO':
+      return [
+        ...state,
+        {
+          id: uuid(),
+          text: action.text,
+          completed: false,
+          createdAt: moment().unix(),
+          completedAt: undefined
+        }
+      ];
+    case 'TOGGLE_TODO':
+      return state.map((todo) => {
+        if (todo.id === action.id) {
+          var nextCompleted = !todo.completed;
+
+          return {
+            id: todo.id,
+            text: todo.text,
+            createdAt: todo.createdAt,
+            completed: nextCompleted,
+            completedAt: nextCompleted ? moment().unix() : undefined
+          };
+        }
+      });
+    default:
+      return state;
+  }
+};
