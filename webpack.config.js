e8bea74fe273a8b0e2e091f1ed2641279bfa4fa1
william-mcdermott <williammcdermott@Williams-MacBook-Pro.local>
2017-01-30 22:27:09 -0600

    add actions for firebase login and logout

--- webpack.config.js
+++ webpack.config.js
@@ -55,7 +55,7 @@ module.exports = {
       applicationStyles: 'app/styles/app.scss',
       actions: 'app/actions/actions.jsx',
       reducers: 'app/reducers/reducers.jsx',
-      configureStore: 'app/store/configureStore.jsx'
+      configureStore: 'app/store/configureStore.jsx',
     },
     extensions: ['', '.js', '.jsx']
   },
